24041a4757ba39845b84f04847a81e42b6f88ba2
balramchoudhary <balramchoudhary314@gmail.com>
2021-12-01 14:16:33 +0530

    now update

--- indexRouter.js
+++ indexRouter.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
     var sql='SELECT * FROM internship_detail';
     client.query(sql, function (err, data, fields) {
     if (err) throw err;
-    console.log(data[0]);
     res.render('user-list', { title: 'User List', userData: data});
   });
 });
